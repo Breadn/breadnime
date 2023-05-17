ed23ed281d362c72e2edfc2b07c4d5d2ad33881e
Breadn <bylueang@gmail.com>
2023-05-17 00:40:20 -0500

    port message

--- app.ts
+++ app.ts
@@ -56,5 +56,5 @@ app.use((req, res) => {
 
 // Listen for connections
 app.listen(port, () => {
-    console.log("Server listening on port 3000");
+    console.log(`Server listening on port ${port}`);
 });
